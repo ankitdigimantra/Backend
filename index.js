52432c84021240b69a0babb2a394233b451296d6
Ankit Singh <ankit.singh@digimantra.com>
2024-05-13 11:14:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require("express");
+
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("Hello twitterererer World!");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Welcome to login here </h1>");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Welcome to YOUTUBE  here </h2>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
